0872873d0dd6fe77b2bcd3d64d7cd01443b4261b
Tomasz Pichlak <tpichlak@gmail.com>
2017-08-29 12:57:17 +0200

    Made a restrictions for data sending to kafka to include only inserts and updates,not deletion operation.

--- src/main/java/InsertDataTrigger.java
+++ src/main/java/InsertDataTrigger.java
@@ -3,6 +3,7 @@ import org.apache.cassandra.db.Clustering;
 import org.apache.cassandra.db.Mutation;
 import org.apache.cassandra.db.partitions.Partition;
 import org.apache.cassandra.db.rows.Cell;
+import org.apache.cassandra.db.rows.Row;
 import org.apache.cassandra.db.rows.Unfiltered;
 import org.apache.cassandra.db.rows.UnfilteredRowIterator;
 import org.apache.cassandra.io.util.FileUtils;
@@ -23,14 +24,12 @@ import java.util.*;
 public class InsertDataTrigger implements ITrigger {
 
     public Collection<Mutation> augment(Partition update) {
-
         //checking if trigger works and some debug info;
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
         System.out.println("Hello " + dateFormat.format(new Date()));
         System.out.println("This Insert Data Trigger");
-        System.out.println("default charset " + Charset.defaultCharset());      //IMPORTANT check if it's important
+        System.out.println("default charset " + Charset.defaultCharset());      //check if it's important
 
-        //here we're gonna build the message to kafka based on inserted data
         try {
             UnfilteredRowIterator it = update.unfilteredIterator();
             CFMetaData cfMetaData = update.metadata();
@@ -39,79 +38,83 @@ public class InsertDataTrigger implements ITrigger {
             System.out.println("update.metadata().clusteringColumns().toString() " + update.metadata().clusteringColumns().toString());
 
             while (it.hasNext()) {
-                JSONObject message = new JSONObject();
-
                 Unfiltered un = it.next();
-                Clustering clt = (Clustering) un.clustering();
 
-                message.put("channel", new String(update.partitionKey().getKey().array()));
+                switch (un.kind()) {    //checking if it's operation per row, other would be range deletion and in this case we won't send info to kafka
 
-                System.out.println("clt.toString(cfMetaData) " + clt.toString(cfMetaData));
-                System.out.println("clt.getRawValues() " + new String(clt.getRawValues()[0].array()));
-                System.out.println("partition.columns().toString() " + update.columns().toString());
+                    case ROW:
+                        JSONObject message = new JSONObject();
 
-                message.put("timestamp", new String(clt.getRawValues()[0].array()));
+                        Clustering clt = (Clustering) un.clustering();
 
-                Iterator<Cell> cells = update.getRow(clt).cells().iterator();
+                        message.put("channel", new String(update.partitionKey().getKey().array()));
 
-//                Iterator<ColumnDefinition> columnDefinitions = partition.getRow(clt).columns().iterator();
-//                while(columnDefinitions.hasNext()){
-//                    ColumnDefinition cDef = columnDefinitions.next();
-//
-//                    System.out.println("cDef.cfName " +  cDef.cfName);
-//                    System.out.println("cDef.ksName " +  cDef.ksName);
-//                    System.out.println("cDef.kind.toString() " +  cDef.kind.toString());
-//                    System.out.println("cDef.name.toString() " +  cDef.name.toString());
-//                    System.out.println("cDef.toString() " + cDef.toString());
-//                }
+                        System.out.println("clt.toString(cfMetaData) " + clt.toString(cfMetaData));
+                        System.out.println("clt.getRawValues() " + new String(clt.getRawValues()[0].array()));
+                        System.out.println("update.columns().toString() " + update.columns().toString());
 
-                while (cells.hasNext()) {
-                    Cell cell = cells.next();
-                    System.out.println("cell.column().name.toString() " + cell.column().name.toString());
-                    //System.out.println("cell.toString()" + cell.toString());
-                    ByteBuffer cellValue = ByteBufferUtil.clone(cell.value());
-                    Float value = cellValue.getFloat();
-                    System.out.println("copied cellValue " + value);
-                    message.put(cell.column().name.toString(), value);
+                        message.put("timestamp", new String(clt.getRawValues()[0].array())); //getting clustering colums of primary key in this case it's only one column timestamp
+
+                        Row row = update.getRow(clt);
+                        if(row.deletion().isLive()) {   //checking if it is not row deletion
+                            //Iterator<Cell> cells = update.getRow(clt).cells().iterator();
+                            Iterator<Cell> cells = row.cells().iterator();
+
+            //                Iterator<ColumnDefinition> columnDefinitions = partition.getRow(clt).columns().iterator();
+            //                while(columnDefinitions.hasNext()){
+            //                    ColumnDefinition cDef = columnDefinitions.next();
+            //
+            //                    System.out.println("cDef.cfName " +  cDef.cfName);
+            //                    System.out.println("cDef.ksName " +  cDef.ksName);
+            //                    System.out.println("cDef.kind.toString() " +  cDef.kind.toString());
+            //                    System.out.println("cDef.name.toString() " +  cDef.name.toString());
+            //                    System.out.println("cDef.toString() " + cDef.toString());
+            //                }
+
+                            while (cells.hasNext()) {
+                                Cell cell = cells.next();
+                                if (!cell.isTombstone()) {
+                                    System.out.println("cell.column().name.toString() " + cell.column().name.toString());
+                                    //System.out.println("cell.toString()" + cell.toString());
+                                    ByteBuffer cellValue = ByteBufferUtil.clone(cell.value());
+                                    Float value = cellValue.getFloat();
+                                    System.out.println("copied cellValue " + value);
+                                    message.put(cell.column().name.toString(), value);
+                                }
+                                else
+                                    message.clear();    //clear message, if cell was deleted we don't want to inform Kafka about that change at least for now
+                            }
+                            //System.out.println("un.toString()" + un.toString(cfMetaData));
+
+                            if (!message.isEmpty()) {
+                                System.out.println(message.toString());
+                                Properties props = loadProperties();
+                                Producer<String, String> producer = new KafkaProducer<>(props, new StringSerializer(), new StringSerializer());
+                                producer.send(new ProducerRecord<>(props.getProperty("topic"), message.toString()));
+                                producer.close();
+                            }
+                        }
+                        break;
                 }
-                //System.out.println("un.toString()" + un.toString(cfMetaData));
-
-                if (!message.isEmpty()) {
-                    System.out.println(message.toString());
-
-                        Properties props = loadProperties();
-                        Producer<String, String> producer = new KafkaProducer<>(props, new StringSerializer(), new StringSerializer());
-                        producer.send(new ProducerRecord<>(props.getProperty("topic"), message.toString()));//move topic name to some properties
-                        producer.close();
-                }
-
-
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
 
         System.out.println("End of trigger");
-        return null;
-        //return Collections.emptyList();
+        return Collections.emptyList();
     }
 
 
-    private static Properties loadProperties()
-    {
-        System.out.println("Loading Properties");
+    private static Properties loadProperties() {
+        //System.out.println("Loading Properties");
         Properties properties = new Properties();
         InputStream stream = InsertDataTrigger.class.getClassLoader().getResourceAsStream("InsertDataTrigger.properties");
-        try
-        {
+        try {
             properties.load(stream);
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             throw new RuntimeException(e);
-        }
-        finally
-        {
+        } finally {
             FileUtils.closeQuietly(stream);
         }
         return properties;
